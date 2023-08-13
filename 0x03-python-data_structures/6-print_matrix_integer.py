f217ec7602abb423f0684ed72198380699f48f76
kelanny <mburukelvin17@gmail.com>
2023-08-13 13:03:34 -0700

    edited task 6 on file 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
-    for lst in matrix[:]:
-        for i in lst[:]:
-            print("{:d}".format(i), end=" ")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
