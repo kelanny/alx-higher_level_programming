f24b118d51a97dabd3fef91b3a8d0876a7e39410
kelanny <mburukelvin17@gmail.com>
2023-08-13 07:46:03 -0700

    Edited and corrected task 1 on file 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, a+b))
-print("{} - {} = {}".format(a, b, a-b))
-print("{} * {} = {}".format(a, b, a*b))
-print("{} / {} = {}".format(a, b, int(a/b)))
+if __name__ == "__main__":
+    """Prints the sum, difference, product and quontient of variable a and b"""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
