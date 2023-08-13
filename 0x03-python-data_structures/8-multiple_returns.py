8d7e55a4796a3109e7cd54979fe14c19e9c90baf
kelanny <mburukelvin17@gmail.com>
2023-08-13 13:27:09 -0700

    Added task 8 file 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """Returns a tuple with length of a string and its first character."""
+    if len(sentence) == 0:
+        sentence[0] = None
+    return (len(sentence), sentence[0])
