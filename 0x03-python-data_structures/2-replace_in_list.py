19d022ae79701e0111f2e6b5d7e688c8f315710b
kelanny <mburukelvin17@gmail.com>
2023-08-13 11:59:47 -0700

    Added task 2 in file 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """Replaces an element of a list at a specific index(like in C)."""
+    if idx < 0:
+        return my_list
+    if idx > len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
