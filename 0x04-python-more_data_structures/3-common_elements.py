287afb8031de8963814760208d84409766dd12a6
kelanny <mburukelvin17@gmail.com>
2023-08-18 17:38:17 -0700

    Edited file 3-common_elements.py for corrections

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,8 +2,4 @@
 # 3-common_elements.py
 
 def common_elements(set_1, set_2):
-    new_common_set = set()
-    for el in set_1:
-        if el in set_2:
-            new_common_set.add(el)
-    return (new_common_set)
+    return (set_1 & set_2)
