cdc563356b97e6e786d9c497f6899105e08b74e0
kelanny <mburukelvin17@gmail.com>
2023-08-28 15:25:12 -0700

    added task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer.
+
+    Args:
+        value (list): The value to print.
+    Returns:
+        True: if correctly printed out.
+        False: if an error occurs.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception:{}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
