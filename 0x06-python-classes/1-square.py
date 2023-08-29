2708cdd702d70c7cc6d2051142e1d7ebefcea6a3
kelanny <mburukelvin17@gmail.com>
2023-08-29 10:44:42 -0700

    Added task 1 on private attribute.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Square."""
+
+
+class Square:
+    """Represent square."""
+
+    def __init__(self, size):
+        """Initializies the square class.
+
+        Args:
+            __size (int): The size of the square sides.
+
+        Returns:
+            Nothing.
+        """
+        self.__size = size
