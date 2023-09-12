8344ab4bb61aebf7d8a532970b18a731f758cafc
kelanny <mburukelvin17@gmail.com>
2023-09-12 15:51:39 -0700

    Added task 0 on file i0.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module defines one function that read text files."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+        print(text)
