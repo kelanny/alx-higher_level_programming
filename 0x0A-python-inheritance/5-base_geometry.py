d7556b7e2d046208f4c2f96a183fa3d713d1faa3
kelanny <mburukelvin17@gmail.com>
2023-09-17 00:06:19 -0700

    Added task 5. Geometry module.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+
+"""
+This module contains an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """An empty Base class representation of BaseGeometry.
+
+    Attributes:
+        None.
+    """
+    pass
