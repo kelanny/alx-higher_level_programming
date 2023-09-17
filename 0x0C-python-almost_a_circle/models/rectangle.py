3461e436d4fa16664062640a8a8392957a0dd54b
kelanny <mburukelvin17@gmail.com>
2023-09-16 21:21:34 -0700

    Added task 2.

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# rectangle.py
+
+"""
+The module contains one class Rectangle.
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    This class represent a rectangle.
+
+    Attributes:
+        id (int): A unique integer that identify an object.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes an instance of a Rectangle Class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): Coordinates of x axis of the top left side of the rect.
+            y (int): Coordingates of y axis.
+            id (int): A number that identify all instances of the Base class.
+        """
+
+        super().__init__(id)
+        self.weight = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Returns the value of private attribute width."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set the value of private attribute width."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the value of private attribute height."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set the value of private attribute height."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Returns the value of private attribute x."""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """Set the value of private attribute x."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Returns the value of private attribute y."""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """Set the value of private attribute y."""
+        self.__y = value
