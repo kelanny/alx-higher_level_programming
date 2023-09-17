5b6e7dec06189d6ca3e9c27a0545ec8049538006
kelanny <mburukelvin17@gmail.com>
2023-09-16 22:12:46 -0700

    Added task 6. __str__.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -152,3 +152,8 @@ class Rectangle(Base):
         for i in range(self.__height):
             [print("#", end="") for _ in range(self.__width)]
             print("")
+
+    def __str__(self):
+        """Returns a string representation of the Rectangle instance.
+        """
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
