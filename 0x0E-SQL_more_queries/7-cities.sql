c7d68f162a5878d296f9bdf993e31d1ccdfdf96e
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-19 05:06:09 +0300

    Added task 7. Cities table a script that creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+--  creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Connect to the database
+USE hbtn_0d_usa;
+
+
+-- Create table "cities"
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(state_id) REFERENCES states(id));
