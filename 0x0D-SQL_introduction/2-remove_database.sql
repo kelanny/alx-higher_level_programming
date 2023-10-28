361167acdc51169493bebb1462877b7cc85d3c42
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-28 03:51:03 +0300

    Added a SQL script that deletes a specific database if it exists.

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,7 @@
+-- Deletes the database hbtn_0c_0 in MySQL Server
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+
+-- You are not allowed to use the SELECT or SHOW statements
+
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
