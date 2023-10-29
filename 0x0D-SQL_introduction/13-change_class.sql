c37b2b33c318d85a80a1f65bb85cab1969f5d7c3
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-29 21:12:30 +0300

    Added SQL script that delete all records with score <= 5 in second_table in hbtn_0c_0 db

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,9 @@
+-- Removes all records with a score <= 5
+-- Table: second_table
+-- Database: hbtn_0c_0
+-- Server: MySQL
+
+-- The database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table
+WHERE score <= 5;
