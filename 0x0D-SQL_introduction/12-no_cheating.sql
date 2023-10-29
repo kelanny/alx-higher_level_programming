3d4ba8f476f369c40dfd48f69cda76969cfd57ab
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-29 21:05:57 +0300

    Added SQL script that update Bob's scores to 10 finding it by name.

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Updates the score of Bob to 10 in the second_table.
+-- You are not allowed to use Bob's id value, just the name.
+-- The db name will be passed as an argument of the mysql command.
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
