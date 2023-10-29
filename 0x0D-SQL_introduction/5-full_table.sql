03e0e13cfab6f77b0d4392930af90ac80c750982
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-29 22:41:16 +0300

    Edited script 5-full_table.sql for corrections.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,7 @@
 -- Prints full description of a table first_table in hbtn_0c_0
 
-DESCRIBE TABLE first_table;
+USE information_schema;
+
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, COLUMN_COMMENT
+FROM COLUMNS
+WHERE TABLE_SCHEMA ='hbtn_0c_0' AND TABLE_NAME = 'first_table';
