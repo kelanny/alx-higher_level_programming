5bc19a051943f8b9a60bde0240403b14df92059a
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-11-16 00:04:21 +0300

    Added a script with add function.

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  const c = a + b;
+  console.log(c);
 }
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-const firstNum = parseInt(arg1);
-const secondNum = parseInt(arg2);
-if (isNaN(firstNum) || isNaN(secondNum)) {
-  console.log('Arguments are not integers.');
-} else {
-  const result = add(firstNum, secondNum);
-  console.log(result);
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
