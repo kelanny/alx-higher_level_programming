d7b722f1222e646cb4b1f7fbc870d8e8883d7ae8
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-11-19 04:30:22 +0300

    Added a python script task 0 that retrieve and display a table using MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,14 +6,14 @@ Database: hbtn_0e_0_usa
 from sys import argv
 import MySQLdb
 
-host = 'localhost'
-user = argv[1]
-pwd = argv[2]
-dbs = argv[3]
 
-db = MySQLdb.connect(host=host, user=user, password=pwd, database=dbs)
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-states = cur.fetchall()
-for state in states:
-    print(state)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+            password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
