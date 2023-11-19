30827e71d3402b923617e5e148a598c4fb6b9bc9
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-11-19 05:12:25 +0300

    Added task 2 python script that list a list of states filtered by user input.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          password=argv[2], database=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4], ))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
