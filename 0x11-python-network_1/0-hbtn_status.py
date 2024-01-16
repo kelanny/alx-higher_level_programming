d5d07f9d552eaf0be83a56f885399172a9821fcb
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-01-15 19:13:29 -0800

    Edited task 0 python file for corrections.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,10 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
+url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     content = response.read()
-
 print("Body response:")
 print("     - type:", type(content))
 print("     - content:", content)
