6b4b2cf6a58600ae911376e10283744e27927b59
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-01-15 18:48:43 -0800

    Edited task 6 a python module that find a peak in unsorted integer list.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Define a function that finds a peak in a list of unsorted integers."""
+
 def find_peak(list_of_integers):
     """
     Find a peak in a list of unsorted integers.
