c7f66047345c7486893e035421595cef12f5808f
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-01-15 16:31:09 -0800

    Edited task 1 '1-body.sh' script for corrections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # send a GET request to the provided URL, and displays the body of the response
-curl -s "$1" -X GET -w "%{http_code}" | awk 'NR>1'  
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+response=$(curl -sL -w "%{http_code}" "$1" -o /dev/null)
+status="${response: -3}"
+
+if [ "$status" -eq 200 ]; then
+	curl -sL "$1"
+fi
