e199cd40f782cb49703b80f03f18b9810948457d
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-01-15 16:02:53 -0800

    Edited task 0-body_size.sh script for corrections

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# takes in a URL, sends a request, and displays the size of the body
+# Check if the URL is provided as an argument
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request using curl and display the body if the status code is 200
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
