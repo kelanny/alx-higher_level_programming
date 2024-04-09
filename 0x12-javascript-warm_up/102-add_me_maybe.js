c43b38ecefdd27586d68d5bd97d72a27087d9f0f
root <root@MBURU-DESKIE>
2024-04-09 04:36:56 +0300

    Added task 16

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function addMeMaybe (number, theFunction) {
+  const incrementedNumber = number + 1;
+  theFunction(incrementedNumber);
+}
+
+module.exports.addMeMaybe = addMeMaybe;
