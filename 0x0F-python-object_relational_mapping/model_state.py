460a2cfb974bcf2b867bdc71d774c3c8b7bb4f2c
root <root@MBURU-DESKIE>
2024-04-15 05:58:49 +0300

    Corrected task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,7 @@ And an instance Base = declarative_base()
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
 
 
@@ -14,12 +15,11 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    
+
     def __str__(self):
         """Return the object in string format"""
         return "{}: {}".format(self.id, self.name)
-    
+
     def __repr__(self):
         """Return the object in string format"""
         return "{}: {}".format(self.id, self.name)
-
